872274c38c2e54ed4ce2006504d641effd98776c
softwaretestingcentre <nicktulett@gmail.com>
2025-07-30 21:01:47 +0100

    Update api.ts

--- src/netlify/functions/api.ts
+++ src/netlify/functions/api.ts
@@ -8,6 +8,12 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
+// Serve the mind-patterns.html file
+router.get('/', (request, resource) => {
+    // Serve mind-patterns.html from src directory
+    resource.sendFile(path.resolve(__dirname, '../../mind-patterns.html'));
+});
+
 api.use("/api/", router);
 api.use(express.static(__dirname + '../../'));
 
