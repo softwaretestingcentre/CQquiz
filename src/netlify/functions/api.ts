de8dabe4a633e54949d696cbe28edc9a6bb1ebeb
Nick Tulett <nicktulett@gmail.com>
2025-07-30 10:09:42 +0100

    netlify express config

---
+++ src/netlify/functions/api.ts
@@ -0,0 +1,14 @@
+// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
+
+import express, { Router } from "express";
+import serverless from "serverless-http";
+
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+api.use(express.static(__dirname + '/'));
+
+export const handler = serverless(api);
