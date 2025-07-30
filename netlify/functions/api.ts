febb850ef8fed45c127358294cead0e8153877a3
softwaretestingcentre <nicktulett@gmail.com>
2025-07-30 21:41:18 +0100

    Update api.ts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -12,11 +12,11 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 // Serve the mind-patterns.html file
 router.get('/', (request, resource) => {
     // Serve mind-patterns.html from src directory
-    resource.sendFile(path.resolve(__dirname, '../../src/mind-patterns.html'));
+    resource.sendFile('/src/mind-patterns.html');
 });
 
 api.use("/api/", router);
-api.use(express.static(__dirname + '../../src/'));
+api.use(express.static('/src/'));
 
 export const handler = serverless(api);
 
