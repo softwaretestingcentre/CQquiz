2ea7e4bbe9168245757d43003deeacfd0d587dcc
NickTulett <nicktulett@yahoo.com>
2025-08-04 17:44:40 +0100

    mind pattern descriptions, gifts and challenges

--- src/mindPatternStepDescriptions.js
+++ src/mindPatternStepDescriptions.js
@@ -86,5 +86,190 @@ export const mindPatternStepDescriptions = {
       <p>I prefer not to be told what to say or have my words filled in for me.</p>
     `,
   },
-
-}
+  VAK: {
+    longDescription: `
+    <p>You naturally "show and tell." You enjoy looking at possibilities - flipping through catalogs, going window-shopping or people-watching 
+    and talking to yourself or someone else about what you see. You make steady eye contact and create a visual impression with the clothes you wear, 
+    which are usually quite colorful and well coordinated. You habitually are attracted to anything in print-novels, billboards, even cereal boxes. 
+    You can keep masses of visual details stored in your mind and remember them easily. Your feelings are written all over your face. 
+    In fact, by looking at your face, others may know how you feel before you are even aware of it.</p>
+    <p>Your words express a lot of energy and are often very persuasive, selling or building a case for something. 
+    You characteristically teach by telling stories in great detail and using many metaphors to paint pictures for your listener, 
+    with your hands emphasizing important points you have just made. When you speak, you use visual vocabulary - words such as 
+    "see", "look", "colourful", "show", and "bright", and phrases such as "I can see your point" and "see you later." 
+    You often use fillers, such as "um", "like", or "you know," between thoughts.</p>`,
+    gifts: `
+    <ul>
+    <li>Whatever you do, underneath it, you are teaching or selling.</li>
+    <li>You illustrate ideas with stories.</li>
+    <li>You write things logically.</li>
+    <li>You have excellent visual recall for faces, what someone was wearing, what something looked like.</li>
+    <li>You can work at visual tasks for long periods of time.</li>
+    <li>You're good at weaving stories that are inspiring or motivating to others.</li>
+    <li>You can hear the whole meaning as well as details in a conversation.</li>
+    </ul>`,
+    challenges: `
+    <ul>
+    <li>You may be numb to the sensations in your body and have a diminished felt sense of aliveness - i.e., you live "in your head."</li>
+    <li>You have to do a physical action over and over in order to learn it.</li>
+    <li>Touch is not casual for you.</li>
+    <li>You can become numb to how you are feeling if you are visually engaged for a long period of time.</li>
+    </ul>`
+  },
+  "VAK": {
+    longDescription: `
+    <p>People are often struck right away by your visual meticulousness. By nature, you are someone who can immediately spot the single typo 
+    in the hundred-page report. How you look is extremely important to you, you want your clothes, body, possessions, and surroundings 
+    to match an inner image you are trying to create. You connect with others most easily by making eye contact. 
+    You can maintain it steadily but may have to close your eyes or move to listen in depth. You instinctively feel what you see.</p>
+    <p>You usually have pent-up energy right beneath the surface and so physical activity tends to be an important emotional outlet for you. 
+    You may be a voracious reader, unless you were taught to read phonetically, in which case you might stick to magazines or newspapers. 
+    You may like to take copious notes in meetings to stay engaged.</p>
+    <p>In large groups, you can be rather quiet. With one or two peers or in small groups, however, you can be very talkative, 
+    jumping from topic to topic. It is typical for you to ask a lot of questions. People may often tell you to "get to the point," 
+    because you may make connections in your speech that may not be understandable to the listener. 
+    Gesturing, moving, or touching is essential to help you find your words.</p>
+    <p>You use lots of visual vocabulary - words that paint images and phrases that include "look", "see", "show", "imagine", 
+    "I can picture that" or "see you soon."</p>`,
+    gifts: `
+    <ul>
+    <li>You have excellent eye-hand and eye-body coordination.</li>
+    <li>You remember easily what you have seen or read.</li>
+    <li>You can recall faces but not necessarily names.</li>
+    <li>You are easily aware of your feelings and the sensations in your body.</li>
+    <li>You pick up the feelings and sensations that are being experienced by others around you.</li>
+    <li>You might be physically active.</li>
+    <li>You have legible handwriting, good spelling, and proofreading ability.</li>
+    <li>You may like to draw and design things in detail.</li>
+    <li>It is easy to write almost word for word what you hear.</li>
+    <li>You prefer working in groups or on teams rather than independently.</li>
+    <li>You hear the whole meaning of something rather than specifics.</li>
+    </ul>`,
+    challenges: `
+    <ul>
+    <li>You don't like to speak spontaneously to large groups of people.</li>
+    <li>Words can take a long time to access.</li>
+    <li>You dislike having words filled in by others.</li>
+    <li>You may find it difficult to concentrate when required to listen for long periods or when asked questions about the details of what you've heard.</li>
+    </ul>`
+  },
+  "KAV": {
+    longDescription: `
+    <p>You are very active in your body and may have shy, sensitive eyes. When given a chance, you most likely prefer to be in constant motion. 
+    Even when sitting, you are rarely still. Your preference is to relate to the world first in some tangible way through action or touch. 
+    You may be a well-coordinated "natural" athlete, who seems to have an endless supply of physical energy.</p>
+    <p>Physical comfort is quite important to you. You may go to great lengths to choose the clothes that feel just right, 
+    as opposed to how they look. Touch comes easily; it is a casual but important way for you to connect. 
+    Your facial expression doesn't typically show how you feel; rather, your body speaks by how you stand, move, or touch.</p>
+    <p>You may use a lot of kinesthetic vocabulary - words and phrases that describe action or feeling, 
+    such as "getting a feel for", "how does that grab you?" or "I can't get a handle on it." 
+    You might end a conversation with "catch you later" or "let's get together soon," and you frequently use 
+    kinesthetic metaphors when you speak: "Let's go for the whole enchilada."</p>`,
+    gifts: `
+    <ul>
+    <li>You have physical stamina.</li>
+    <li>You are a competent hands-on "doer," who prefers to be on your feet and in action.</li>
+    <li>You do things in a logical and systematic manner. Movements are strong, steady, direct, and precise.</li>
+    <li>You enjoy telling stories of what you've done, how you did it, and how it felt to do it.</li>
+    <li>You see the whole of something.</li>
+    <li>You're able to access and verbalize body sensations in a specific, organized way. You can tell exactly where your head hurts or which muscle in your leg is pulled.</li>
+    <li>You like to work with your hands-for example, tinkering with cars, working with wood or crafts.</li>
+    <li>You are skilled at teaching people how to do things, since translating action into words is easy.</li>
+    <li>You may be keenly interested in how things work, from complex machines to the human body.</li>
+    </ul>`,
+    challenges: `
+    <ul>
+    <li>You're overwhelmed by too much eye contact or visuals, such as detailed visual diagrams or instructions.</li>
+    <li>You need to move and jiggle, you may have trouble sitting still for long periods of time.</li>
+    <li>Casual touch, which comes naturally to you, may be offensive to others.</li>
+    <li>"Nasty" looks - glances that indicate judgment or criticism - can be more painful than physical or verbal punishments.</li>
+    </ul>`
+  },
+  "KVA": {
+    longDescription: `
+    <p>You are soft-spoken and private, with a grounded physical presence. You may be interested in seemingly diverse things: 
+    football and art, for instance, or sewing and chemistry. You instinctively search for how things come together or can be integrated.</p>
+    <p>It's easy for you to be aware of the specific sensations in your body. You know very clearly, for instance, 
+    if a shirt tag is rubbing your neck. You tend to choose clothes that are comfortable and allow freedom of movement. 
+    If you are in a group, you will often look for a quiet place to sit back, watch, and listen. Commonly labeled "shy," 
+    you enjoy working or playing alone or with one close friend and many times find it easier to relate to animals or nature 
+    than to people. Your eyes glaze over if you listen to too many words, and your facial expression usually goes flat when 
+    you speak. You frequently use kinesthetic vocabulary, words that convey action or feelings, like "trying to get a feel for this" 
+    or "need to get a handle on it" or "I'll be in touch soon."</p>`,
+    gifts: `
+    <ul>
+    <li>You're a deep listener and ask large questions to evoke the whole of what is being said.</li>
+    <li>You do things logically and systematically.</li>
+    <li>You can visualize a three-dimensional image with your eyes open, such as imagining a giraffe from any angle.</li>
+    <li>You are able to see things from many perspectives, including the validity of many sides of an issue.</li>
+    <li>You can see the whole of something as well as its details.</li>
+    <li>You hear the whole of something, such as harmonies.</li>
+    <li>You can spend long periods being physically active.</li>
+    <li>You may have good eye-hand coordination and like working with your hands and creating in three dimensions. You're easily able to put things together, sometimes in very creative ways.</li>
+    <li>You can acquire new physical skills with ease.</li>
+    </ul>`,
+    challenges: `
+    <ul>
+    <li>You don't speak comfortably in a detailed way to large groups of people.</li>
+    <li>Words may take a long time to access; you dislike having your words filled in by others.</li>
+    <li>You can get overwhelmed by too much conversation.</li>
+    <li>You're very sensitive to tones of voice.</li>
+    </ul>`
+  },
+  "AVK": {
+    longDescription: `
+    <p>You are often considered "smart" because you can easily verbalize what you think and keep up 
+    with the pace of any conversation. Your words naturally pour out in logical order without hesitation. 
+    You tend to speak in statements rather than questions. The content of your speech is detailed and specific. 
+    Saying something precisely is very important to you. You may love verbal humor, are fascinated with language, 
+    and can often learn to speak other languages with ease. Most likely you enjoy explaining, debating, discussing, 
+    and arguing about almost anything. You may frequently use words and phrases such as "hear", "say", "sounds like", 
+    "understand", "that rings a bell", "let's play it by ear", and "talk to you soon."</p>`,
+    gifts: `
+    <ul>
+    <li>You can explain ideas verbally.</li>
+    <li>You're good at giving speeches, verbal reports, or participating in discussions of all sorts.</li>
+    <li>You see the big picture and the details of a project at the same time.</li>
+    <li>You remember precisely what was said.</li>
+    </ul>`,
+    challenges: `
+    <ul>
+    <li>You're often shy about expressing feelings or being touched.</li>
+    <li>You dislike physical humor, such as tickling or pranks.</li>
+    <li>You can have trouble doing hands-on, technical skills.</li>
+    <li>You may have a tendency to interrupt others in meetings and monopolize conversations.</li>
+    </ul>`
+  },
+  "AKV": {
+    longDescription: `
+    <p>You are articulate and have a high degree of physical energy right beneath the surface. 
+    You naturally love to take charge and tell everyone else what to do. 
+    Your habit is to discuss, argue, or debate anything, tell jokes, and make plays on words. 
+    You understand and make verbal inferences easily and respond quickly to questions. 
+    You may have a distinctive, one-of-a-kind voice and speak clearly, precisely, and with a lot of energy, 
+    feeling, and rhythm. You frequently use words and phrases such as "hear", "say", "sounds like", "that rings a bell", 
+    "let's play it by ear", and "talk to you soon."</p>
+    <p>You perhaps are able to repeat what you've heard word-for-word, in tape-recorder fashion. 
+    This includes poetry, song lyrics, rhymes, and jokes. You can have strong feelings and opinions, 
+    which you readily express verbally.</p>
+    <p>You seem to have an endless supply of physical energy. 
+    You easily learn physical movement, even if given only verbal instructions. 
+    You can be very particular about the visual images you choose movies, television shows, 
+    and room decorations since you are deeply influenced by what you see.</p>`,
+    gifts: `
+    <ul>
+    <li>You are a natural leader and coach and love to tell others what to do.</li>
+    <li>You see the whole of something.</li>
+    <li>You may be a great visionary thinker.</li>
+    <li>You're great at giving speeches, verbal reports, or participating in discussions of all sorts.</li>
+    <li>You can easily teach someone else what to do, without hesitation.</li>
+    </ul>`,
+    challenges: `
+    <ul>
+    <li>You might have trouble listening and may interrupt others, especially if you are feeling a lot of excitement.</li>
+    <li>You can come across as sarcastic.</li>
+    <li>You prefer a low amount of visual input and dislike detailed writing, diagrams, or instructions.</li>
+    <li>You may have a great deal of pent-up energy right beneath the surface.</li>
+    </ul>`
+  },
+};
